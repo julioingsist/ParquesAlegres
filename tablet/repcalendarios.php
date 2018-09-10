b1f063bc14e1040c35b8466448b83b41ef75de9f
Julio César Castro Beltrán <julioc.castro.beltran@gmail.com>
2018-09-09 20:58:34 -0600

    se regresan consultas a mysql 5.6

--- tablet/repcalendarios.php
+++ tablet/repcalendarios.php
@@ -7,16 +7,16 @@ $evidencias = array(0 => "No", 1 => "Fotos");
 
 $sql = "SELECT a.ID,u.display_name FROM asesores AS a INNER JOIN wp_users AS u ON a.ID = u.ID 
 		WHERE stat < 1";
-$res = mysqli_query($enlace, $sql);
-while ($row = mysqli_fetch_array($res)) {
+$res = mysql_query($sql);
+while ($row = mysql_fetch_array($res)) {
 	$asesores[$row['ID']] = $row['display_name'];
 }
 
 $sql = "SELECT p.ID, p.post_title FROM wp_posts p INNER JOIN asesores a ON a.ID = p.post_author 
 	     WHERE p.post_status = 'publish' AND p.post_type = 'parque' AND a.stat < 1 
 	     ORDER BY p.post_title ASC";
-$res = mysqli_query($enlace, $sql);
-while ($row = mysqli_fetch_array($res)) {
+$res = mysql_query($sql);
+while ($row = mysql_fetch_array($res)) {
 	$parques[$row['ID']] = $row['post_title'];
 }
 
@@ -35,10 +35,10 @@ if ($_GET['fecha_fin'] != "") {
 if ($_POST['cmd'] == 2) {
 	$sql = "SELECT id, post_title FROM wp_posts WHERE post_author = '".$_POST['asesor']."' AND
 			post_status='publish' AND post_type='parque' ORDER BY post_title ASC";
-	$res = mysqli_query($enlace, $sql);
-	if (mysqli_num_rows($res) > 0){
+	$res = mysql_query($sql);
+	if (mysql_num_rows($res) > 0){
 		echo '<option value=""> -- Todos --</option>';
-		while ($row = mysqli_fetch_array($res)){
+		while ($row = mysql_fetch_array($res)){
 			echo '<option value="'.$row['id'].'">'.$row['post_title'].'</option>';
 		}
 	} else {
@@ -86,8 +86,8 @@ if ($_POST['cmd'] == 1) {
     		$filtro
     		ORDER BY fecha_registro";
 	
-	$res = mysqli_query($enlace, $sql);
-	if (mysqli_num_rows($res) > 0) {
+	$res = mysql_query($sql);
+	if (mysql_num_rows($res) > 0) {
 		echo '<table>
 		<tr>
 			<td>Fecha Registro</td>
@@ -101,7 +101,7 @@ if ($_POST['cmd'] == 1) {
 			<td>Evidencia</td>
 		</tr>';
 
-		while ($row = mysqli_fetch_array($res)) {
+		while ($row = mysql_fetch_array($res)) {
 			echo '<tr>
 				  <td>'.$row['fecha_registro'].
 				  		'<input type="hidden" name="fecha_registro[]" value="'.$row['fecha_registro'].
@@ -149,7 +149,7 @@ if ($_POST['cmd'] == 1) {
         	echo '</td>';
         	echo '</tr>';
         }
-        echo '<tr><td><b>Total:</b></td><td colspan="10"><b>'.mysqli_num_rows($res).'</b></td></table>';
+        echo '<tr><td><b>Total:</b></td><td colspan="10"><b>'.mysql_num_rows($res).'</b></td></table>';
 		echo '</table>';
 	} else {
 		echo 'No hay calendarios registrados bajo el criterio de búsqueda.';
@@ -371,7 +371,7 @@ input[type="text"]{
         var fecha_fin = document.getElementsByName("fecha_fin")[0].value;
         var tiene_calendario = document.getElementsByName("tiene_calendario")[0].value;
         var tiene_evidencia = document.getElementsByName("tiene_evidencia")[0].value;
-		$("#resultados").load("http://localhost/web-site/tablet/repcalendarios.php", {asesor: asesor,
+		$("#resultados").load("http://parquesalegres.org/tablet/repcalendarios.php", {asesor: asesor,
 		 	parque: parque, fecha_inicial: fecha_inicial, fecha_fin: fecha_fin, 
 		 	tiene_calendario: tiene_calendario, tiene_evidencia: tiene_evidencia, cmd: 1});
     }
